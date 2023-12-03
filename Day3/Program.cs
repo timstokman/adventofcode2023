3ff7f7436609162d5a4e4e7fbcc4fa814bce0aae
Tim Stokman <timstokman@gmail.com>
2023-12-03 07:43:29 +0100

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+using Common;
+
+string puzzleInput = await Util.GetPuzzleInput(3);
+string[] puzzleLines = puzzleInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+Regex numberMatch = new Regex(@"\d+");
+IEnumerable<(int lineIndex, Match match)> numberMatches = puzzleLines.SelectMany((line, lineIndex) =>
+{
+   var matches = numberMatch.Matches(line);
+   return matches.Select(m => (lineIndex, m));
+});
+IEnumerable<int> partNumbers = numberMatches.Where(i =>
+{
+   (int lineIndex, var match) = i;
+   var indexesToCheck = new List<(int Y, int X)>()
+   {
+      ( lineIndex, match.Index - 1 ), 
+      ( lineIndex - 1, match.Index - 1 ), 
+      ( lineIndex + 1, match.Index - 1 ),
+
+      ( lineIndex, match.Index + match.Length ), 
+      ( lineIndex - 1, match.Index + match.Length ),
+      ( lineIndex + 1, match.Index + match.Length )
+   };
+   indexesToCheck.AddRange(Enumerable.Range(match.Index, match.Length).Select(i => (lineIndex + 1, i)));
+   indexesToCheck.AddRange(Enumerable.Range(match.Index, match.Length).Select(i => (lineIndex - 1, i)));
+   var symbolIndexes = indexesToCheck.Where(toCheck => toCheck.X >= 0 && toCheck.X < puzzleLines[0].Length && toCheck.Y >= 0 && toCheck.Y < puzzleLines.Length && puzzleLines[toCheck.Y][toCheck.X] != '.' && !char.IsDigit(puzzleLines[toCheck.Y][toCheck.X]));
+   return symbolIndexes.Any();
+}).Select(num => int.Parse(num.match.Value));
+int sumPartNumbers = partNumbers.Sum();
+Regex gearMatch = new Regex(@"\*");
+IEnumerable<(int lineIndex, Match match)> gearMatches = puzzleLines.SelectMany((line, lineIndex) =>
+{
+   var matches = gearMatch.Matches(line);
+   return matches.Select(m => (lineIndex, m));
+});
+IEnumerable<int> gearRatios = gearMatches.Select(i =>
+{
+   (int lineIndex, var match) = i;
+   var indexesToCheck = new List<(int Y, int X)>()
+   {
+      (lineIndex, match.Index - 1),
+      (lineIndex - 1, match.Index - 1),
+      (lineIndex + 1, match.Index - 1),
+
+      (lineIndex + 1, match.Index),
+      (lineIndex - 1, match.Index),
+
+      (lineIndex, match.Index + match.Length),
+      (lineIndex - 1, match.Index + match.Length),
+      (lineIndex + 1, match.Index + match.Length)
+   };
+   var digitIndexes = indexesToCheck.Where(toCheck => toCheck.X >= 0 && toCheck.X < puzzleLines[0].Length && toCheck.Y >= 0 && toCheck.Y < puzzleLines.Length && char.IsDigit(puzzleLines[toCheck.Y][toCheck.X]));
+   var matchingDigits = digitIndexes.Select(matchingDigit => ((int lineIndex, Match m)?)numberMatches.FirstOrDefault(numberMatch => matchingDigit.X >= numberMatch.match.Index && matchingDigit.X < numberMatch.match.Index + numberMatch.match.Length && numberMatch.lineIndex == matchingDigit.Y)).Where(m => m != null).DistinctBy(item => (item.Value.lineIndex, item.Value.m.Index)).ToList();
+   if (matchingDigits.Count() == 2)
+   {
+      return int.Parse(matchingDigits.First().Value.m.Value) * int.Parse(matchingDigits.Last().Value.m.Value);
+   }
+   else
+   {
+      return 0;
+   }
+});
+int sumGearRatios = gearRatios.Sum();
+
+Console.WriteLine(sumPartNumbers);
+Console.WriteLine(sumGearRatios);
