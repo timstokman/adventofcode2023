63b8999f7f0aa0439e618b076ae1254a743d0fb0
Tim Stokman <timstokman@gmail.com>
2023-12-04 07:30:31 +0100

    Tweak

--- Day4/Card.cs
+++ Day4/Card.cs
@@ -4,10 +4,10 @@ public record Card(int Number, List<int> WinningNumbers, List<int> OwningNumbers
 {
     public static Card FromLine(string line)
     {
-        var split = line.Split(": ");
+        string[] split = line.Split(": ");
         int num = int.Parse(split[0][5..]);
-        var splitSections = split[1].Split(" | ", StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries);
-        return new Card(num, splitSections[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList(), splitSections[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+        string[] splitSections = split[1].Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+        return new Card(num, splitSections[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList(), splitSections[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
     }
 
     public IEnumerable<int> MatchingNumbers
