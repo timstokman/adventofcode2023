af8ca6a1f10f68f04fc0065627a077aa34c0f444
Tim Stokman <timstokman@gmail.com>
2023-12-07 07:57:58 +0100

    day7p1

---
+++ Day7/Program.cs
@@ -0,0 +1,9 @@
+﻿using Common;
+using Day7;
+using Microsoft.VisualBasic.CompilerServices;
+
+string puzzle = await Util.GetPuzzleInput(7);
+var rounds = puzzle.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Round.FromLine).ToList();
+rounds.Sort();
+int winnings = rounds.Select((round, i) => (i + 1) * round.Bid).Sum();
+Console.WriteLine(winnings);
---
+++ Day7/Round.cs
@@ -0,0 +1,100 @@
+namespace Day7;
+
+public record Round(char[] Hand, int Bid) : IComparable<Round>
+{
+    public static char[] CardStrength = { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+    public static Round FromLine(string line)
+    {
+        var split = line.Split(" ");
+        return new Round(split[0].ToCharArray(), int.Parse(split[1]));
+    }
+
+    public Dictionary<char, int> Frequency { get; } = Hand.GroupBy(h => h).ToDictionary(h => h.Key, h => h.Count());
+
+    public IEnumerable<int> FrequencySorted() => Frequency.Select(f => f.Value).OrderDescending();
+    
+    public bool IsFiveOfAKind()
+        => FrequencySorted().First() == 5;
+
+    public bool IsFourOfAKind()
+        => FrequencySorted().First() == 4;
+
+    public bool IsFullHouse()
+        => FrequencySorted().SequenceEqual(new[] { 3, 2 });
+
+    public bool IsThreeOfAKind()
+        => FrequencySorted().SequenceEqual(new[] { 3, 1, 1 });
+
+    public bool IsTwoPair()
+        => FrequencySorted().SequenceEqual(new[] { 2, 2, 1 });
+
+    public bool IsOnePair()
+        => FrequencySorted().SequenceEqual(new[] { 2, 1, 1, 1 });
+
+    public bool IsHighCard()
+        => Frequency.Count() == 5;
+
+    public int Type()
+    {
+        if (IsFiveOfAKind())
+        {
+            return 6;
+        }
+        else if (IsFourOfAKind())
+        {
+            return 5;
+        }
+        else if (IsFullHouse())
+        {
+            return 4;
+        }
+        else if (IsThreeOfAKind())
+        {
+            return 3;
+        }
+        else if (IsTwoPair())
+        {
+            return 2;
+        }
+        else if (IsOnePair())
+        {
+            return 1;
+        }
+        else if (IsHighCard())
+        {
+            return 0;
+        }
+        else
+        {
+            throw new Exception("What?");
+        }
+    }
+
+    public int CompareTo(Round? other)
+    {
+        if (other == null)
+        {
+            return -1;
+        }
+
+        int typeDiff = Type() - other.Type();
+        if (typeDiff != 0)
+        {
+            return typeDiff;
+        }
+        else
+        {
+            for (int i = 0; i < Hand.Length; i++)
+            {
+                int cardDiff =  Array.IndexOf(CardStrength, other.Hand[i]) - Array.IndexOf(CardStrength, Hand[i]);
+                if (cardDiff != 0)
+                {
+                    return cardDiff;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
