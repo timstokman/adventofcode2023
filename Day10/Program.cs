73d11730bd12ee2e5c72002e41df621b4fd760e1
Tim Stokman <timstokman@gmail.com>
2023-12-10 08:34:53 +0100

    Checkpoint

---
+++ Day10/Program.cs
@@ -0,0 +1,167 @@
+﻿using Common;
+using Microsoft.VisualBasic.CompilerServices;
+
+Dictionary<char, (int X, int Y)[]> connectors = new()
+{
+    { '|', new []{ (0, -1), (0, 1) }},
+    { '-', new []{ (-1, 0), (1, 0) }},
+    { 'L', new []{ (0, -1), (1, 0) }},
+    { 'J', new []{ (0, -1), (-1, 0) }},
+    { 'F', new []{ (0, 1), (1, 0) }},
+    { '7', new []{ (0, 1), (-1, 0) }},
+    { 'S', new[] { (0, -1), (0, 1), (-1, 0), (1, 0) }},
+};
+
+(int X, int Y)[] GetConnectingPipes(char[][] map, (int x, int y) position)
+{
+    if (connectors.TryGetValue(map[position.y][position.x], out var connections))
+    {
+        return connections.Where(connection =>
+        {
+            (int netX, int netY) = (position.x + connection.X, position.y + connection.Y);
+            if (netX >= map.Length || netX < 0 || netY >= map.Length || netY < 0)
+            {
+                return false;
+            }
+
+            return connectors.ContainsKey(map[netY][netX]) && connectors[map[netY][netX]].Any(backConnection => connection.X == -1 * backConnection.X && connection.Y == -1 * backConnection.Y);
+        }).Select(connection =>
+        {
+            return (X: position.x + connection.X, Y: position.y + connection.Y);
+        }).ToArray();
+    }
+    else
+    {
+        return new (int X, int Y)[] { };
+    }
+}
+
+IEnumerable<(int X, int Y)> GetLoop(char[][] map, (int X, int Y) start, (int X, int Y) next)
+{
+    yield return start;
+    var previous = start;
+    while (true)
+    {
+        yield return next;
+        var connecting = GetConnectingPipes(map, next).Where(c => c != previous).ToArray();
+        if (connecting.Length != 1)
+        {
+            break;
+        }
+        
+        previous = next;
+        next = connecting.First();
+
+        if (next == start)
+        {
+            yield return start;
+            break;
+        }
+    }
+}
+
+(int X, int Y)[] FindLargestConnectingLoop(char[][] map)
+{
+    int startX = 0;
+    int startY = 0;
+    for (startY = 0; startY < map.Length; startY++)
+    {
+        startX = Array.IndexOf(map[startY], 'S');
+        if (startX >= 0)
+        {
+            break;
+        }
+    }
+
+    (int X, int Y)[][] loops = GetConnectingPipes(map, (startX, startY)).Select(connection => GetLoop(map, (startX, startY), (connection.X, connection.Y)).ToArray()).Where(l => l.Last() == (startX, startY)).ToArray();
+    int maxLength = loops.Max(l => l.Count());
+    return loops.First(l => l.Count() == maxLength).Take(maxLength - 1).ToArray();
+}
+
+int CountInsideLoop(char[][] map, (int X, int Y)[] loop)
+{
+    HashSet<(int X, int Y)> loopSet = new(loop);
+    HashSet<(int X, int Y)> outsideLoop = new();
+
+    IEnumerable<(int X, int Y)> ToIterate(int mapLength, int side, int depth)
+    {
+        (int startX, int startY, int endX, int endY, int directionX, int directionY) = (0, 0, 0, 0, 0, 0);
+        switch (side)
+        {
+            case 0:
+                startX = depth;
+                startY = depth;
+                endX = depth;
+                endY = mapLength - depth;
+                directionY = 1;
+                break;
+            case 1:
+                startX = depth;
+                startY = depth;
+                endX = mapLength - depth;
+                endY = depth;
+                directionX = 1;
+                break;
+            case 2:
+                startX = mapLength - depth;
+                startY = mapLength - depth;
+                endX = depth;
+                endY = mapLength - depth;
+                directionX = -1;
+                break;
+            case 3:
+                startX = mapLength - depth;
+                startY = mapLength - depth;
+                endX = mapLength - depth;
+                endY = depth;
+                directionY = -1;
+                break;
+            default:
+                throw new Exception();
+        }
+
+        var current = (X: startX, Y: startY);
+        while (true)
+        {
+            yield return current;
+            if (current == (endX, endY))
+            {
+                break;
+            }
+            current = (current.X + directionX, current.Y + directionY);
+        }
+    }
+
+    (int X, int Y)[] Sides = { (-1, 0), (1, 0), (0, 1), (0, -1) };
+
+    for (int depth = 0; depth < map.Length / 2; depth++)
+    {
+        for (int side = 0; side < 4; side++)
+        {
+            foreach (var marker in ToIterate(map.Length, side, depth))
+            {
+                if (!loopSet.Contains(marker) && Sides.Any(s => outsideLoop.Contains((marker.X + s.X, marker.Y + s.Y))))
+                {
+                    outsideLoop.Add(marker);
+                }
+            }
+        }
+    }
+    return (map.Length * map.Length) - loop.Length - outsideLoop.Count;
+}
+
+string puzzleInput = @"...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+
+char[][] map = puzzleInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(l => l.ToCharArray()).ToArray();
+var largestLoop = FindLargestConnectingLoop(map);
+int insideLoop = CountInsideLoop(map, largestLoop);
+Console.WriteLine(largestLoop.Length / 2);
+Console.WriteLine(insideLoop);
