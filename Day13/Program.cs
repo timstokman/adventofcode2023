b84e8b435c21e33b78d6593e96da61c27778de80
Tim Stokman <timstokman@gmail.com>
2023-12-14 02:04:37 +0100

    Simplify

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -10,32 +10,33 @@ string[] TransposePattern(string[] pattern)
                          Enumerable.Range(0, pattern.Length)
                                    .Select(newColumnIndex => pattern[newColumnIndex][newRowIndex]).ToArray())).ToArray();
 
-IEnumerable<int> VerticalMirrorIndex(string[] pattern, int smudge = 1)
+IEnumerable<int> VerticalMirrorIndex(string[] pattern, int smudge)
 {
-    Console.WriteLine(string.Join(Environment.NewLine, pattern));
     return Enumerable.Range(0, pattern[0].Length - 1).Where(mirrorIndex =>
     {
         int rangeToCheck = Math.Min(mirrorIndex + 1, pattern[0].Length - mirrorIndex - 1);
-        int numNotMirrored = Enumerable.Range(0, pattern.Length).Sum(verticalIndex =>
+        int numNotMirrored = Enumerable
+            .Range(0, pattern.Length)
+            .Sum(verticalIndex =>
             {
                 string left = pattern[verticalIndex][(mirrorIndex + 1 - rangeToCheck)..(mirrorIndex + 1)];
                 string right = Reverse(pattern[verticalIndex][(mirrorIndex + 1)..(mirrorIndex + 1 + rangeToCheck)]);
                 return Enumerable.Range(0, left.Length).Count(i => left[i] != right[i]);
-            }
-        );
+            });
         return numNotMirrored == smudge;
     });
 }
 
-IEnumerable<int> HorizontalMirrorIndex(string[] pattern)
-    => VerticalMirrorIndex(TransposePattern(pattern));
+IEnumerable<int> HorizontalMirrorIndex(string[] pattern, int smudge)
+    => VerticalMirrorIndex(TransposePattern(pattern), smudge);
 
-int SummarizePattern(string[] pattern, int i)
-{
-    return VerticalMirrorIndex(pattern).Sum(v => v + 1) + HorizontalMirrorIndex(pattern).Sum(h => (h + 1) * 100);
-}
+int SummarizePattern(string[] pattern, int smudge)
+    => VerticalMirrorIndex(pattern, smudge).Sum(v => v + 1) + HorizontalMirrorIndex(pattern, smudge).Sum(h => (h + 1) * 100);
 
 string puzzleInput = await Util.GetPuzzleInput(13);
 
 string [][] patterns = puzzleInput.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(p => p.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)).ToArray();
-Console.WriteLine(patterns.Select(SummarizePattern).Sum());
+int summary = patterns.Sum(pattern => SummarizePattern(pattern, 0));
+int summarySmudge = patterns.Sum(pattern => SummarizePattern(pattern, 1));
+Console.WriteLine($"Summary: {summary}");
+Console.WriteLine($"Summary with smudge: {summarySmudge}");
