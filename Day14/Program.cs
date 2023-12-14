12c6e59d05c4125012476988201e9cd5647284ca
Tim Stokman <timstokman@gmail.com>
2023-12-14 07:45:21 +0100

    In between, not there yet

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,10 +1,20 @@
 ﻿using Common;
 
-string puzzleInput = await Util.GetPuzzleInput(14);
+string puzzleInput = @"
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
 
 char[][] TiltNorth(char[][] map)
 {
-    var newMap = map.Select(row => row.Select(c => c is '.' or '#' ? c : '.').ToArray()).ToArray();
+    char[][] newMap = map.Select(row => row.Select(c => c is '.' or '#' ? c : '.').ToArray()).ToArray();
     for (int column = 0; column < map[0].Length; column++)
     {
         int[] solidRockIndexes = new[] { -1}.Concat(map.Select((rock, row) => (rock, row)).Where(r => r.rock[column] == '#').Select(r => r.row)).Concat(new[] { map.Length }).ToArray();
@@ -24,6 +34,54 @@ char[][] TiltNorth(char[][] map)
     return newMap;
 }
 
+char[][] TurnClockwise(char[][] map)
+{
+    char[][] newMap = Enumerable.Range(0, map[0].Length).Select(r => Enumerable.Range(0, map.Length).Select(i => ' ').ToArray()).ToArray();
+
+    for (int r = 0; r < map.Length; r++)
+    {
+        for (int c = 0; c < map[0].Length; c++)
+        {
+            newMap[c][map.Length - r - 1] = map[r][c];
+        }
+    }
+
+    return newMap;
+}
+
+char[][] RunCycle(char[][] map)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        map = TiltNorth(map);
+        map = TurnClockwise(map);
+    }
+
+    return map;
+}
+
+char[][] RunCycles(char[][] map, int cycles)
+{
+    string MapToString(char[][] m)
+        => string.Join("", m.Select(r => string.Join("", r)));
+    
+    List<string> history = new() { MapToString(map) };
+
+    for (int i = 0; i < cycles; i++)
+    {
+        map = RunCycle(map);
+        string mapStr = MapToString(map);
+        int beforeIndex = history.IndexOf(mapStr);
+        if (beforeIndex >= 0)
+        {
+            Console.WriteLine("here!");
+        }
+        history.Append(mapStr);
+    }
+
+    return map;
+}
+
 int Load(char[][] map)
     => map.Select((row, rowIndex) => (map.Length - rowIndex) * row.Count(r => r == 'O')).Sum();
 
@@ -31,7 +89,9 @@ void Print(char[][] map)
     => Console.WriteLine(string.Join(Environment.NewLine, map.Select(r => string.Join("", r))));
 
 char[][] map = puzzleInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(line => line.ToCharArray()).ToArray();
-var north = TiltNorth(map);
-Print(north);
-int load = Load(north);
-Console.WriteLine(load);
+// var north = TiltNorth(map);
+// Print(north);
+// int load = Load(north);
+// Console.WriteLine(load);
+map = RunCycles(map, 1000000000);
+Console.WriteLine(Load(map));
