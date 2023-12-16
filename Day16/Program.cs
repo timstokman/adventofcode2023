32a624bc515192712ab515dd53d6ffb7f3c8b819
Tim Stokman <timstokman@gmail.com>
2023-12-16 07:49:12 +0100

    Make prettier

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,6 +1,80 @@
 ﻿using Common;
 using Position = (int X, int Y);
 
+IEnumerable<(Position Position, Direction Direction)> NextPositions(char[][] chars, Position position, Direction direction)
+{
+    switch (chars[position.Y][position.X])
+    {
+        case '.':
+            var newPosition = direction switch
+            {
+                Direction.Top => new Position(position.X, position.Y - 1),
+                Direction.Right => new Position(position.X + 1, position.Y),
+                Direction.Bottom => new Position(position.X, position.Y + 1),
+                Direction.Left => new Position(position.X - 1, position.Y),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            yield return (newPosition, direction);
+            break;
+        case '/':
+            yield return direction switch
+            {
+                Direction.Top => (new Position(position.X + 1, position.Y), Direction.Right),
+                Direction.Right => (new Position(position.X, position.Y - 1), Direction.Top),
+                Direction.Bottom => (new Position(position.X - 1, position.Y), Direction.Left),
+                Direction.Left => (new Position(position.X, position.Y + 1), Direction.Bottom),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            break;
+        case '\\':
+            yield return direction switch
+            {
+                Direction.Top => (new Position(position.X - 1, position.Y), Direction.Left),
+                Direction.Right => (new Position(position.X, position.Y + 1), Direction.Bottom),
+                Direction.Bottom => (new Position(position.X + 1, position.Y), Direction.Right),
+                Direction.Left => (new Position(position.X, position.Y - 1), Direction.Top),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            break;
+        case '|':
+            if (direction == Direction.Top || direction == Direction.Bottom)
+            {
+                var newPositionVerticalSplit = direction switch
+                {
+                    Direction.Top => new Position(position.X, position.Y - 1),
+                    Direction.Bottom => new Position(position.X, position.Y + 1),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                yield return (newPositionVerticalSplit, direction);
+            }
+            else
+            {
+                yield return (new Position(position.X, position.Y - 1), Direction.Top);
+                yield return (new Position(position.X, position.Y + 1), Direction.Bottom);
+            }
+            break;
+        case '-':
+            if (direction == Direction.Left || direction == Direction.Right)
+            {
+                var newPositionHorizontalSplit = direction switch
+                {
+                    Direction.Right => new Position(position.X + 1, position.Y),
+                    Direction.Left => new Position(position.X - 1, position.Y),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                yield return (newPositionHorizontalSplit, direction);
+            }
+            else
+            {
+                yield return (new Position(position.X - 1, position.Y), Direction.Left);
+                yield return (new Position(position.X + 1, position.Y), Direction.Right);
+            }
+            break;
+        default:
+            throw new ArgumentOutOfRangeException();
+    }
+}
+
 IEnumerable<((int X, int Y) Position, Direction Direction)> PossibleEntryPositions(int height, int width)
 {
     return Enumerable.Range(0, height).Select(i => (new Position(i, 0), Direction.Bottom)).Concat(
@@ -13,8 +87,8 @@ int TrackEnergize(char[][] map, Position startPosition, Direction startDirection
 {
     int height = map.Length;
     int width = map[0].Length;
-    Dictionary<Position, List<Direction>> nodeStates = new Dictionary<Position, List<Direction>>();
-    Queue<(Position Position, Direction Direction)> queue = new Queue<(Position Position, Direction Direction)>();
+    Dictionary<Position, List<Direction>> nodeStates = new();
+    Queue<(Position Position, Direction Direction)> queue = new();
     queue.Enqueue((startPosition, startDirection));
 
     while (queue.Any())
@@ -41,77 +115,9 @@ int TrackEnergize(char[][] map, Position startPosition, Direction startDirection
             nodeState.Add(direction);
         }
 
-        switch (map[position.Y][position.X])
+        foreach (var next in NextPositions(map, position, direction))
         {
-            case '.':
-                var newPosition = direction switch
-                {
-                    Direction.Top => new Position(position.X, position.Y - 1),
-                    Direction.Right => new Position(position.X + 1, position.Y),
-                    Direction.Bottom => new Position(position.X, position.Y + 1),
-                    Direction.Left => new Position(position.X - 1, position.Y),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                queue.Enqueue((newPosition, direction));
-                break;
-            case '/':
-                ((int X, int Y), Direction) nextRightMirror = direction switch
-                {
-                    Direction.Top => (new Position(position.X + 1, position.Y), Direction.Right),
-                    Direction.Right => (new Position(position.X, position.Y - 1), Direction.Top),
-                    Direction.Bottom => (new Position(position.X - 1, position.Y), Direction.Left),
-                    Direction.Left => (new Position(position.X, position.Y + 1), Direction.Bottom),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                queue.Enqueue(nextRightMirror);
-                break;
-            case '\\':
-                ((int X, int Y), Direction) nextLeftMirror = direction switch
-                {
-                    Direction.Top => (new Position(position.X - 1, position.Y), Direction.Left),
-                    Direction.Right => (new Position(position.X, position.Y + 1), Direction.Bottom),
-                    Direction.Bottom => (new Position(position.X + 1, position.Y), Direction.Right),
-                    Direction.Left => (new Position(position.X, position.Y - 1), Direction.Top),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                queue.Enqueue(nextLeftMirror);
-                break;
-            case '|':
-                if (direction == Direction.Top || direction == Direction.Bottom)
-                {
-                    var newPositionVerticalSplit = direction switch
-                    {
-                        Direction.Top => new Position(position.X, position.Y - 1),
-                        Direction.Bottom => new Position(position.X, position.Y + 1),
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-                    queue.Enqueue((newPositionVerticalSplit, direction));
-                }
-                else
-                {
-                    queue.Enqueue((new Position(position.X, position.Y - 1), Direction.Top));
-                    queue.Enqueue((new Position(position.X, position.Y + 1), Direction.Bottom));
-                }
-                break;
-            case '-':
-                if (direction == Direction.Left || direction == Direction.Right)
-                {
-                    var newPositionHorizontalSplit = direction switch
-                    {
-                        Direction.Right => new Position(position.X + 1, position.Y),
-                        Direction.Left => new Position(position.X - 1, position.Y),
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-                    queue.Enqueue((newPositionHorizontalSplit, direction));
-                }
-                else
-                {
-                    queue.Enqueue((new Position(position.X - 1, position.Y), Direction.Left));
-                    queue.Enqueue((new Position(position.X + 1, position.Y), Direction.Right));
-                }
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
+            queue.Enqueue(next);
         }
     }
 
