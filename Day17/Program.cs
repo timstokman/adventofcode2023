c51f8e9657db61d40a7a4e2a2de2c47b907c8fe1
Tim Stokman <timstokman@gmail.com>
2023-12-17 08:26:14 +0100

    Minor cleanup

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,6 +1,27 @@
 ﻿using Common;
 using Point = (int X, int Y, Direction? LastDirection);
 
+Direction? Inverse(Direction? direction)
+{
+    return direction switch
+    {
+        Direction.Top => Direction.Bottom,
+        Direction.Right => Direction.Left,
+        Direction.Bottom => Direction.Top,
+        Direction.Left => Direction.Right,
+        null => null,
+        _ => throw new ArgumentOutOfRangeException()
+    };
+}
+
+(int, int, Direction)[] surrounding =
+{
+    (-1, 0, Direction.Left),
+    (1, 0, Direction.Right),
+    (0, -1, Direction.Top),
+    (0, 1, Direction.Bottom),
+};
+
 int ShortestPath(int[][] nodeCosts, int minStraightLine = 4, int maxStraightLine = 10)
 {
     int[,,] dist = new int[nodeCosts.Length, nodeCosts[0].Length, 4];
@@ -28,23 +49,8 @@ int ShortestPath(int[][] nodeCosts, int minStraightLine = 4, int maxStraightLine
     {
         IEnumerable<(Point Neighbour, int Cost)> Neighbours(Point point)
         {
-            var surrounding = new (int X, int Y, Direction Direction)[]
-            {
-                (-1, 0, Direction.Left),
-                (1, 0, Direction.Right),
-                (0, -1, Direction.Top),
-                (0, 1, Direction.Bottom),
-            };
-            Direction? inverse = point.LastDirection switch
-            {
-                Direction.Top => Direction.Bottom,
-                Direction.Right => Direction.Left,
-                Direction.Bottom => Direction.Top,
-                Direction.Left => Direction.Right,
-                null => null,
-                _ => throw new ArgumentOutOfRangeException()
-            };
-            foreach (var s in surrounding)
+            Direction? inverse = Inverse(point.LastDirection);
+            foreach ((int X, int Y, Direction Direction) s in surrounding)
             {
                 if (s.Direction != point.LastDirection && s.Direction != inverse)
                 {
@@ -66,46 +72,15 @@ int ShortestPath(int[][] nodeCosts, int minStraightLine = 4, int maxStraightLine
             }
         }
 
-        IEnumerable<Point> Path(Point last)
-        {
-            Point c = last;
-            while (prev.ContainsKey(c))
-            {
-                yield return c;
-                c = prev[c];
-            }
-        }
-
         Point u = queue.MinBy(kv => kv.Value).Key;
         queue.Remove(u);
 
         if (u.Y == nodeCosts.Length - 1 && u.X == nodeCosts[0].Length - 1)
         {
-            var path = Path(u).Reverse().ToArray();
-            int cost = dist[u.Y, u.X, (int)(u.LastDirection ?? Direction.Top)];
-
-            for (int y = 0; y < nodeCosts.Length; y++)
-            {
-                for (int x = 0; x < nodeCosts[0].Length; x++)
-                {
-                    if (path.Any(p => p.X == x && p.Y == y))
-                    {
-                        Console.Write("#");
-                    }
-                    else
-                    {
-                        Console.Write(nodeCosts[y][x]);
-                    }
-                }
-
-                Console.WriteLine();
-            }
-            
-            return cost;
+            return dist[u.Y, u.X, (int)(u.LastDirection ?? Direction.Top)];
         }
 
-        var neighbours = Neighbours(u);
-        foreach ((Point v, int cost) in neighbours)
+        foreach ((Point v, int cost) in Neighbours(u))
         {
             int alt = dist[u.Y, u.X, (int)(u.LastDirection ?? Direction.Top)] + cost;
             if (alt < dist[v.Y, v.X, (int)(v.LastDirection!)])
@@ -123,4 +98,8 @@ int ShortestPath(int[][] nodeCosts, int minStraightLine = 4, int maxStraightLine
 string puzzleInput = await Util.GetPuzzleInput(17);
 
 int[][] nodeCosts = puzzleInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(l => l.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
-Console.WriteLine(ShortestPath(nodeCosts));
+int minPathCrucible = ShortestPath(nodeCosts, 1, 3);
+int minPathUltraCrucible = ShortestPath(nodeCosts, 4, 10);
+
+Console.WriteLine($"Least heat crucible: {minPathCrucible}");
+Console.WriteLine($"Least heat ultra-crucible: {minPathUltraCrucible}");
